316920a5a3bb00b35a3208dceda95e73a734f2b3
Tim Schlechter <timschlechter@gmail.com>
2014-05-08 02:22:50 +0200

    Moved pulgins to subfolder

--- nuget/CreatePackages.cmd
+++ nuget/CreatePackages.cmd
@@ -2,8 +2,8 @@ del *.nupkg
 
 nuget pack ..\Specflow.Reporting\Specflow.Reporting.csproj -Build -Properties Configuration=Release
 
-nuget pack ..\Specflow.Reporting.PlainText\Specflow.Reporting.PlainText.csproj -Build -Properties Configuration=Release
+nuget pack ..\plugins\Specflow.Reporting.PlainText\Specflow.Reporting.PlainText.csproj -Build -Properties Configuration=Release
 
-nuget pack ..\Specflow.Reporting.Json\Specflow.Reporting.Json.csproj -Build -Properties Configuration=Release
+nuget pack ..\plugins\Specflow.Reporting.Json\Specflow.Reporting.Json.csproj -Build -Properties Configuration=Release
 
-nuget pack ..\Specflow.Reporting.Xml\Specflow.Reporting.Xml.csproj -Build -Properties Configuration=Release
+nuget pack ..\plugins\Specflow.Reporting.Xml\Specflow.Reporting.Xml.csproj -Build -Properties Configuration=Release
