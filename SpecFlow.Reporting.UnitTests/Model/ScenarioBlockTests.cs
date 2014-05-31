52d893297208c204d927ea61607a14ed6de94572
Tim Schlechter <timschlechter@gmail.com>
2014-05-31 22:35:04 +0200

    Added UnitTest project

---
+++ SpecFlow.Reporting.UnitTests/Model/ScenarioBlockTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlow.Reporting.UnitTests.Model
+{
+    [TestFixture]
+    public class ScenarioBlockTests
+    {
+        [Test]
+        public void Result_WhenContainsNoSteps_ReturnsOK()
+        {
+            var scenarioblock = new ScenarioBlock();
+
+            Assert.AreEqual(TestResult.OK, scenarioblock.Result);
+        }
+
+        [Test]
+        public void Result_WhenContainsOKStep_ReturnsOK()
+        {
+            var scenarioblock = new ScenarioBlock();
+
+            scenarioblock.Steps.Add(new Step { Result = TestResult.OK });
+
+            Assert.AreEqual(TestResult.OK, scenarioblock.Result);
+        }
+
+        [Test]
+        public void Result_WhenContainsOKAndErrorStep_ReturnsError()
+        {
+            var scenarioblock = new ScenarioBlock();
+
+            scenarioblock.Steps.Add(new Step { Result = TestResult.OK });
+            scenarioblock.Steps.Add(new Step { Result = TestResult.Error });
+
+            Assert.AreEqual(TestResult.Error, scenarioblock.Result);
+        }
+    }
+}
--- SpecFlow.Reporting/Model/Step.cs
+++ SpecFlow.Reporting/Model/Step.cs
@@ -4,6 +4,11 @@ namespace SpecFlow.Reporting
 {
 	public class Step : ReportItem
 	{
+        public Step()
+        {
+            Steps = new List<Step>();
+        }
+
 		public List<Step> Steps { get; set; }
 
 		public TableParam Table { get; set; }
